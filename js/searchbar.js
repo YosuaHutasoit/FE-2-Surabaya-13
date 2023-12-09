288278fbc40a086cf48eecf9e6c013ebcb5ff72f
udinnproyeksi <Mujahidin1122211@gmail.com>
2023-12-09 16:18:31 +0800

    menambahkan pop up pada lending page , menamabhkan font, ubah harga hotel ,

--- js/searchbar.js
+++ js/searchbar.js
@@ -11,3 +11,24 @@ const swiper = new Swiper(".swiper", {
     slidesPerView: "auto",
     spaceBetween: 20,
 });
+
+var popup = document.querySelector(".popup");
+var dismiss = document.querySelector(".popup__dismiss");
+var shop = document.querySelector(".popup__shop");
+
+setTimeout(function() {
+    popup.classList.add("popup--show");
+}, 3000);
+
+dismiss.addEventListener("click", function() {
+    console.log("kil dismiss");
+    popup.classList.remove("popup--show");
+    popup.classList.add("popup--close");
+})
+
+shop.addEventListener("click", function() {
+    console.log("kil shop");
+    popup.classList.remove("popup--show");
+    popup.classList.add("popup--close");
+    alert("Selamat anda dapat diskon");
+})
